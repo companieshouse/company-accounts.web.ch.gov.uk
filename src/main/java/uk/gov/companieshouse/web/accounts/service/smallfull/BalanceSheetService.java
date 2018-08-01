f6a4822aab2d97b965da0052c2a08830d50fe727
drosser <drosser@companieshouse.gov.uk>
2018-08-01 12:37:13 +0100

    Add basic interface to BalanceSheetServiceImpl and BalanceSheetTransformer.
    - Add basic interface to BalanceSheetServiceImpl that controller will use
    - Add basic interface to BalanceSheetTransformer that service will use
    - Update pom to import JUnit Jupiter dependency and Mockito dependency
    - Add basic unit tests for implementations of service and transformer
    
    Resolves: SFA-43.

--- src/main/java/uk/gov/companieshouse/web/accounts/service/smallfull/BalanceSheetService.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/service/smallfull/BalanceSheetService.java
@@ -1,4 +1,9 @@
 package uk.gov.companieshouse.web.accounts.service.smallfull;
 
+
+import uk.gov.companieshouse.web.accounts.model.smallfull.BalanceSheet;
+
 public interface BalanceSheetService {
+
+    BalanceSheet getBalanceSheet(String companyId, String transactionId, String accountsId);
 }
