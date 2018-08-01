d400c37823a4a32b4b8ad9285013d5a990efa0ef
drosser <drosser@companieshouse.gov.uk>
2018-08-01 11:29:52 +0100

    Remove un-needed NotNull annotation.
    Resolves: SFA-43.

--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/CalledUpShareCapitalNotPaid.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/CalledUpShareCapitalNotPaid.java
@@ -4,13 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Range;
 
-import javax.validation.constraints.NotNull;
-
 @Getter
 @Setter
 public class CalledUpShareCapitalNotPaid {
 
-    @NotNull
     @Range(min = 0, max = 99999999, message = "{invalidRange}")
     private Integer currentAmount;
 }
