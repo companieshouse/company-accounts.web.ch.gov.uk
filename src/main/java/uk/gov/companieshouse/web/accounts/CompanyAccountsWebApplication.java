c21e3d5d6be25220882eac0938ffebf149a704b2
bsaunders95 <bsaunders@companieshouse.gov.uk>
2018-08-07 20:47:55 +0100

    Add session handler to the request filter chain. Add user details interceptor which executes before a view renders to retrieve the email from the session and append it to the model. Update the user bar fragment to display the email from the model.

--- src/main/java/uk/gov/companieshouse/web/accounts/CompanyAccountsWebApplication.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/CompanyAccountsWebApplication.java
@@ -1,12 +1,28 @@
 package uk.gov.companieshouse.web.accounts;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import uk.gov.companieshouse.web.accounts.interceptor.UserDetailsInterceptor;
 
 @SpringBootApplication
-public class CompanyAccountsWebApplication {
+public class CompanyAccountsWebApplication implements WebMvcConfigurer {
+
+    UserDetailsInterceptor userDetailsInterceptor;
+
+    @Autowired
+    public CompanyAccountsWebApplication(UserDetailsInterceptor userDetailsInterceptor) {
+        this.userDetailsInterceptor = userDetailsInterceptor;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CompanyAccountsWebApplication.class, args);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userDetailsInterceptor);
     }
 }
