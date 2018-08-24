af0b39020ba57beb62e6de42de4f76d152c103bc
bsaunders95 <bsaunders@companieshouse.gov.uk>
2018-08-24 08:32:09 +0100

    Add @Service annotation to services

--- src/main/java/uk/gov/companieshouse/web/accounts/service/company/impl/CompanyServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/service/company/impl/CompanyServiceImpl.java
@@ -1,12 +1,14 @@
 package uk.gov.companieshouse.web.accounts.service.company.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.api.ApiClient;
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
 import uk.gov.companieshouse.web.accounts.api.ApiClientService;
 import uk.gov.companieshouse.web.accounts.service.company.CompanyService;
 
+@Service
 public class CompanyServiceImpl implements CompanyService {
 
     @Autowired
