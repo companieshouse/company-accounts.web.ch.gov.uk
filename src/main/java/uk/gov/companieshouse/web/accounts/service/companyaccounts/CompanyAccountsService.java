bee260ce61f14974487e71f2432efd784dd644f8
bsaunders95 <bsaunders@companieshouse.gov.uk>
2018-08-28 10:11:47 +0100

    Alter parameter types and return type of the createCompanyAccounts method for encapsulation of knowledge

--- src/main/java/uk/gov/companieshouse/web/accounts/service/companyaccounts/CompanyAccountsService.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/service/companyaccounts/CompanyAccountsService.java
@@ -1,10 +1,11 @@
 package uk.gov.companieshouse.web.accounts.service.companyaccounts;
 
+import com.google.api.client.util.DateTime;
 import java.util.Date;
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.model.accounts.CompanyAccountsApi;
 
 public interface CompanyAccountsService {
 
-    CompanyAccountsApi createCompanyAccounts(String transactionId, Date periodEndOn) throws ApiErrorResponseException;
+    String createCompanyAccounts(String transactionId, DateTime periodEndOn) throws ApiErrorResponseException;
 }
