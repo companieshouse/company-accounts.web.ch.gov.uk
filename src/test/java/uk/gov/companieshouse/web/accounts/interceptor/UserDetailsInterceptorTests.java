ee4209d24019f90926b86cf50afab4fee60e7b75
Marc Ransome <mransome@companieshouse.gov.uk>
2018-09-05 13:11:14 +0100

    Replace on-demand import with explicit imports

--- src/test/java/uk/gov/companieshouse/web/accounts/interceptor/UserDetailsInterceptorTests.java
+++ src/test/java/uk/gov/companieshouse/web/accounts/interceptor/UserDetailsInterceptorTests.java
@@ -19,7 +19,10 @@ import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.never;
 
 @ExtendWith(MockitoExtension.class)
 public class UserDetailsInterceptorTests {
