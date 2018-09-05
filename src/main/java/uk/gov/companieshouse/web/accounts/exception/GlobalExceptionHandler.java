6f53f7b68c7d7d14f25ad5dd5a923611ac60182a
Marc Ransome <mransome@companieshouse.gov.uk>
2018-09-05 12:56:00 +0100

    Remove unused import

--- src/main/java/uk/gov/companieshouse/web/accounts/exception/GlobalExceptionHandler.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/exception/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 import uk.gov.companieshouse.web.accounts.CompanyAccountsWebApplication;
