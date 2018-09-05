7920bb3c70de40d3f9f367789dc98fb582eb1838
bsaunders95 <bsaunders@companieshouse.gov.uk>
2018-09-05 10:40:02 +0100

    Add transactions service Javadocs

--- src/main/java/uk/gov/companieshouse/web/accounts/service/transaction/TransactionService.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/service/transaction/TransactionService.java
@@ -4,7 +4,21 @@ import uk.gov.companieshouse.web.accounts.exception.ServiceException;
 
 public interface TransactionService {
 
+    /**
+     * Create a CHS transaction record. A transaction is comprised of one or more filing resources.
+     * Once the status is set to 'closed', the transaction filings will be processed by CHIPS.
+     *
+     * @param companyNumber The company number for which accounts are to be filed
+     * @return The ID of the created transaction
+     * @throws ServiceException
+     */
     String createTransaction(String companyNumber) throws ServiceException;
 
+    /**
+     * Set the status to 'closed' for an accounts filing transaction
+     *
+     * @param transactionId The ID of the transaction to close
+     * @throws ServiceException
+     */
     void closeTransaction(String transactionId) throws ServiceException;
 }
