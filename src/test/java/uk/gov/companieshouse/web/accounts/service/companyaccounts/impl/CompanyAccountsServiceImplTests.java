6aff923e012e40e92ea33409cbf5b89a8d7db4f5
Marc Ransome <mransome@companieshouse.gov.uk>
2018-09-06 12:08:34 +0100

    Add company accounts service success and failure tests

--- src/test/java/uk/gov/companieshouse/web/accounts/service/companyaccounts/impl/CompanyAccountsServiceImplTests.java
+++ src/test/java/uk/gov/companieshouse/web/accounts/service/companyaccounts/impl/CompanyAccountsServiceImplTests.java
@@ -3,6 +3,8 @@ package uk.gov.companieshouse.web.accounts.service.companyaccounts.impl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.google.api.client.util.DateTime;
@@ -20,11 +22,16 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.companieshouse.api.ApiClient;
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.transaction.TransactionResourceHandler;
+import uk.gov.companieshouse.api.handler.transaction.companyaccount.CompanyAccountResourceHandler;
 import uk.gov.companieshouse.api.handler.transaction.companyaccount.CompanyAccountsResourceHandler;
+import uk.gov.companieshouse.api.handler.transaction.companyaccount.smallfull.SmallFullResourceHandler;
 import uk.gov.companieshouse.api.model.accounts.CompanyAccountsApi;
+import uk.gov.companieshouse.api.model.accounts.smallfull.SmallFullApi;
 import uk.gov.companieshouse.web.accounts.api.ApiClientService;
 import uk.gov.companieshouse.web.accounts.exception.ServiceException;
 import uk.gov.companieshouse.web.accounts.service.companyaccounts.CompanyAccountsService;
+
+import javax.xml.ws.Service;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -42,6 +49,12 @@ public class CompanyAccountsServiceImplTests {
     @Mock
     private CompanyAccountsResourceHandler companyAccountsResourceHandler;
 
+    @Mock
+    private SmallFullResourceHandler smallFullResourceHandler;
+
+    @Mock
+    private CompanyAccountResourceHandler companyAccountResourceHandler;
+
     @InjectMocks
     private CompanyAccountsService companyAccountsService = new CompanyAccountsServiceImpl();
 
@@ -55,14 +68,13 @@ public class CompanyAccountsServiceImplTests {
         when(apiClientService.getApiClient()).thenReturn(apiClient);
 
         when(apiClient.transaction(TRANSACTION_ID)).thenReturn(transactionResourceHandler);
-
-        when(transactionResourceHandler.companyAccounts())
-                .thenReturn(companyAccountsResourceHandler);
     }
 
     @Test
     @DisplayName("Create Company Accounts - Success Path")
     void createCompanyAccountSuccess() throws ServiceException, ApiErrorResponseException {
+
+        when(transactionResourceHandler.companyAccounts()).thenReturn(companyAccountsResourceHandler);
 
         DateTime periodEndOn = new DateTime(new Date());
 
@@ -80,8 +92,10 @@ public class CompanyAccountsServiceImplTests {
     }
 
     @Test
-    @DisplayName("Get Company Profile - Throws ApiErrorResponseException")
-    void getBalanceSheetThrowsApiErrorResponseException() throws ApiErrorResponseException {
+    @DisplayName("Create Company Accounts - Failure Path")
+    void createCompanyAccountFailure() throws ServiceException, ApiErrorResponseException {
+
+        when(transactionResourceHandler.companyAccounts()).thenReturn(companyAccountsResourceHandler);
 
         DateTime periodEndOn = new DateTime(new Date());
 
@@ -91,4 +105,40 @@ public class CompanyAccountsServiceImplTests {
         assertThrows(ServiceException.class, () ->
                 companyAccountsService.createCompanyAccounts(TRANSACTION_ID, periodEndOn));
     }
+
+    @Test
+    @DisplayName("Create Small Full Accounts - Success Path")
+    void createSmallFullSuccess() throws ServiceException, ApiErrorResponseException {
+
+        when(transactionResourceHandler.companyAccount(COMPANY_ACCOUNTS_ID))
+                .thenReturn(companyAccountResourceHandler);
+
+        when(companyAccountResourceHandler.smallFull()).thenReturn(smallFullResourceHandler);
+
+        SmallFullApi smallFullApi = new SmallFullApi();
+
+        when(smallFullResourceHandler.create()).thenReturn(smallFullApi);
+
+        companyAccountsService.createSmallFullAccounts(TRANSACTION_ID, COMPANY_ACCOUNTS_ID);
+
+        verify(smallFullResourceHandler, times(1)).create();
+    }
+
+    @Test
+    @DisplayName("Create Small Full Accounts - Failure Path")
+    void createSmallFullFailure() throws ServiceException, ApiErrorResponseException {
+
+        when(transactionResourceHandler.companyAccount(COMPANY_ACCOUNTS_ID))
+                .thenReturn(companyAccountResourceHandler);
+
+        when(companyAccountResourceHandler.smallFull()).thenReturn(smallFullResourceHandler);
+
+        when(smallFullResourceHandler.create())
+                .thenThrow(ApiErrorResponseException.class);
+
+        assertThrows(ServiceException.class, () ->
+                companyAccountsService.createSmallFullAccounts(TRANSACTION_ID, COMPANY_ACCOUNTS_ID));
+    }
+
+
 }
