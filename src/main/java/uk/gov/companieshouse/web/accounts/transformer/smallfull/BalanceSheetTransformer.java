5bc1d5bec78461b861c824ff58a162e90925851a
ChrisJBurns <ChrisJBurns@users.noreply.github.com>
2018-10-02 11:58:50 +0100

    adds previous transformer

--- src/main/java/uk/gov/companieshouse/web/accounts/transformer/smallfull/BalanceSheetTransformer.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/transformer/smallfull/BalanceSheetTransformer.java
@@ -1,5 +1,6 @@
 package uk.gov.companieshouse.web.accounts.transformer.smallfull;
 
+import uk.gov.companieshouse.api.model.accounts.abridged.PreviousPeriodApi;
 import uk.gov.companieshouse.api.model.accounts.smallfull.CurrentPeriodApi;
 import uk.gov.companieshouse.web.accounts.model.smallfull.BalanceSheet;
 
@@ -8,4 +9,11 @@ public interface BalanceSheetTransformer {
     BalanceSheet getBalanceSheet(CurrentPeriodApi currentPeriod);
 
     CurrentPeriodApi getCurrentPeriod(BalanceSheet balanceSheet);
+
+    /**
+     * Gets the previous period data on the balance sheet
+     * @param balanceSheet populated balance sheet
+     * @return populated previous period object
+     */
+    PreviousPeriodApi getPreviousPeriod(BalanceSheet balanceSheet);
 }
