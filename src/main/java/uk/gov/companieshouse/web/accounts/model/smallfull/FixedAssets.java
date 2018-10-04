fee816e213103991c32f0bab2eb1571e78c761cf
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-10-04 06:51:35 +0100

    Add validation mapping annotation to smallfull model for field validation

--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/FixedAssets.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/FixedAssets.java
@@ -2,6 +2,7 @@ package uk.gov.companieshouse.web.accounts.model.smallfull;
 
 import lombok.Getter;
 import lombok.Setter;
+import uk.gov.companieshouse.web.accounts.validation.ValidationMapping;
 
 @Getter
 @Setter
@@ -9,6 +10,7 @@ public class FixedAssets {
 
     private TangibleAssets tangibleAssets;
 
+    @ValidationMapping("$.current_period.balance_sheet.fixed_assets.total")
     private Integer totalFixedAssets;
 
 }
--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/TangibleAssets.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/TangibleAssets.java
@@ -2,11 +2,13 @@ package uk.gov.companieshouse.web.accounts.model.smallfull;
 
 import lombok.Getter;
 import lombok.Setter;
+import uk.gov.companieshouse.web.accounts.validation.ValidationMapping;
 
 @Getter
 @Setter
 public class TangibleAssets {
 
+    @ValidationMapping("$.current_period.balance_sheet.fixed_assets.tangible")
     private Integer currentAmount;
 
 }
