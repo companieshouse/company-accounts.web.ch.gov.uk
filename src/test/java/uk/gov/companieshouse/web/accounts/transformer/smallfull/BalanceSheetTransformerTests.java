257b81d94fd6c106fc3009a61ca8f24f5de459f9
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-10-04 09:12:42 +0100

    Add fixed assets to unit tests

--- src/test/java/uk/gov/companieshouse/web/accounts/transformer/smallfull/BalanceSheetTransformerTests.java
+++ src/test/java/uk/gov/companieshouse/web/accounts/transformer/smallfull/BalanceSheetTransformerTests.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import uk.gov.companieshouse.api.model.accounts.smallfull.BalanceSheetApi;
 import uk.gov.companieshouse.api.model.accounts.smallfull.CurrentPeriodApi;
+import uk.gov.companieshouse.api.model.accounts.smallfull.FixedAssetsApi;
 import uk.gov.companieshouse.web.accounts.model.smallfull.BalanceSheet;
 import uk.gov.companieshouse.web.accounts.model.smallfull.CalledUpShareCapitalNotPaid;
+import uk.gov.companieshouse.web.accounts.model.smallfull.FixedAssets;
+import uk.gov.companieshouse.web.accounts.model.smallfull.TangibleAssets;
 import uk.gov.companieshouse.web.accounts.transformer.smallfull.impl.BalanceSheetTransformerImpl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,6 +17,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class BalanceSheetTransformerTests {
 
     private Long CALLED_UP_SHARE_CAPITAL = Long.valueOf("1");
+    private Long TANGIBLE = Long.valueOf("5");
+    private Long FIXED_ASSETS_TOTAL = Long.valueOf("5");
 
     private BalanceSheetTransformer transformer = new BalanceSheetTransformerImpl();
 
@@ -24,6 +29,12 @@ public class BalanceSheetTransformerTests {
         BalanceSheetApi currentPeriodBalanceSheetApi = new BalanceSheetApi();
         currentPeriodBalanceSheetApi.setCalledUpShareCapitalNotPaid(CALLED_UP_SHARE_CAPITAL);
 
+        FixedAssetsApi fixedAssetsApi = new FixedAssetsApi();
+        fixedAssetsApi.setTangibleApi(TANGIBLE);
+        fixedAssetsApi.setTotal(FIXED_ASSETS_TOTAL);
+
+        currentPeriodBalanceSheetApi.setFixedAssetsApi(fixedAssetsApi);
+
         CurrentPeriodApi currentPeriod = new CurrentPeriodApi();
         currentPeriod.setBalanceSheetApi(currentPeriodBalanceSheetApi);
 
@@ -32,6 +43,8 @@ public class BalanceSheetTransformerTests {
         assertNotNull(balanceSheet);
         assertNotNull(balanceSheet.getCalledUpShareCapitalNotPaid());
         assertEquals(CALLED_UP_SHARE_CAPITAL, balanceSheet.getCalledUpShareCapitalNotPaid().getCurrentAmount());
+        assertEquals(TANGIBLE, balanceSheet.getFixedAssets().getTangibleAssets().getCurrentAmount());
+        assertEquals(FIXED_ASSETS_TOTAL, balanceSheet.getFixedAssets().getTotalCurrentFixedAssets());
     }
 
     @Test
@@ -49,5 +62,26 @@ public class BalanceSheetTransformerTests {
         assertNotNull(currentPeriod);
         assertNotNull(currentPeriod.getBalanceSheetApi());
         assertEquals(CALLED_UP_SHARE_CAPITAL, currentPeriod.getBalanceSheetApi().getCalledUpShareCapitalNotPaid());
+    }
+
+    @Test
+    @DisplayName("Get Current Period - Assert Fixed Assets Are Correct")
+    void getCurrentPeriodFixedAssets() {
+
+        FixedAssets fixedAssets = new FixedAssets();
+        TangibleAssets tangible = new TangibleAssets();
+        tangible.setCurrentAmount(TANGIBLE);
+        fixedAssets.setTangibleAssets(tangible);
+        fixedAssets.setTotalCurrentFixedAssets(FIXED_ASSETS_TOTAL);
+
+        BalanceSheet balanceSheet = new BalanceSheet();
+        balanceSheet.setFixedAssets(fixedAssets);
+
+        CurrentPeriodApi currentPeriod = transformer.getCurrentPeriod(balanceSheet);
+
+        assertNotNull(currentPeriod);
+        assertNotNull(currentPeriod.getBalanceSheetApi());
+        assertEquals(TANGIBLE, currentPeriod.getBalanceSheetApi().getFixedAssetsApi().getTangibleApi());
+        assertEquals(FIXED_ASSETS_TOTAL, currentPeriod.getBalanceSheetApi().getFixedAssetsApi().getTotal());
     }
 }
