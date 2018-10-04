81a18ec95e4f6d138022d806670af0d18f4fe900
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-10-04 06:52:19 +0100

    Add fixed assets to transformer class

--- src/main/java/uk/gov/companieshouse/web/accounts/transformer/smallfull/impl/BalanceSheetTransformerImpl.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/transformer/smallfull/impl/BalanceSheetTransformerImpl.java
@@ -1,10 +1,14 @@
 package uk.gov.companieshouse.web.accounts.transformer.smallfull.impl;
 
 import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.api.model.accounts.abridged.balancesheet.TangibleApi;
 import uk.gov.companieshouse.api.model.accounts.smallfull.BalanceSheetApi;
 import uk.gov.companieshouse.api.model.accounts.smallfull.CurrentPeriodApi;
+import uk.gov.companieshouse.api.model.accounts.smallfull.FixedAssetsApi;
 import uk.gov.companieshouse.web.accounts.model.smallfull.BalanceSheet;
 import uk.gov.companieshouse.web.accounts.model.smallfull.CalledUpShareCapitalNotPaid;
+import uk.gov.companieshouse.web.accounts.model.smallfull.FixedAssets;
+import uk.gov.companieshouse.web.accounts.model.smallfull.TangibleAssets;
 import uk.gov.companieshouse.web.accounts.transformer.smallfull.BalanceSheetTransformer;
 
 @Component
@@ -19,9 +23,17 @@ public class BalanceSheetTransformerImpl implements BalanceSheetTransformer {
 
         CalledUpShareCapitalNotPaid calledUpShareCapitalNotPaid = new CalledUpShareCapitalNotPaid();
         calledUpShareCapitalNotPaid.setCurrentAmount(
-                currentPeriodBalanceSheetApi.getCalledUpShareCapitalNotPaid());
+             currentPeriodBalanceSheetApi.getCalledUpShareCapitalNotPaid());
+
+        TangibleAssets tangibleAssets = new TangibleAssets();
+        tangibleAssets.setCurrentAmount(currentPeriodBalanceSheetApi.getFixedAssetsApi().getTangibleApi().getValue());
+
+        FixedAssets fixedAssets = new FixedAssets();
+        fixedAssets.setTotalFixedAssets(currentPeriodBalanceSheetApi.getFixedAssetsApi().getTotal());
+        fixedAssets.setTangibleAssets(tangibleAssets);
 
         balanceSheet.setCalledUpShareCapitalNotPaid(calledUpShareCapitalNotPaid);
+        balanceSheet.setFixedAssets(fixedAssets);
 
         return balanceSheet;
     }
@@ -30,10 +42,18 @@ public class BalanceSheetTransformerImpl implements BalanceSheetTransformer {
     public CurrentPeriodApi getCurrentPeriod(BalanceSheet balanceSheet) {
 
         BalanceSheetApi balanceSheetApi = new BalanceSheetApi();
+        FixedAssetsApi fixedAssetsApi = new FixedAssetsApi();
+        TangibleApi tangibleApi = new TangibleApi();
+
+        tangibleApi.setValue(balanceSheet.getFixedAssets().getTangibleAssets().getCurrentAmount());
+
+        fixedAssetsApi.setTangibleApi(tangibleApi);
+        fixedAssetsApi.setTotal(balanceSheet.getFixedAssets().getTotalFixedAssets());
 
         balanceSheetApi.setCalledUpShareCapitalNotPaid(
                 balanceSheet.getCalledUpShareCapitalNotPaid().getCurrentAmount());
 
+        balanceSheetApi.setFixedAssetsApi(fixedAssetsApi);
         CurrentPeriodApi currentPeriod = new CurrentPeriodApi();
 
         currentPeriod.setBalanceSheetApi(balanceSheetApi);
