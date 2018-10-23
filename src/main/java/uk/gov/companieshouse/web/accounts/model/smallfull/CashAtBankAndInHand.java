96d4834c8e4316d0fcce27fb91fffa26f99b7562
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-10-23 10:21:38 +0100

    Add current assets model to company accounts web

--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/CashAtBankAndInHand.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/CashAtBankAndInHand.java
@@ -2,12 +2,16 @@ package uk.gov.companieshouse.web.accounts.model.smallfull;
 
 import lombok.Getter;
 import lombok.Setter;
+import uk.gov.companieshouse.web.accounts.validation.ValidationMapping;
 
 @Getter
 @Setter
 public class CashAtBankAndInHand {
 
+    @ValidationMapping("$.current_period.balance_sheet.current_assets.cash_at_bank_and_in_hand")
     private Long currentAmount;
+
+    @ValidationMapping("$.previous_period.balance_sheet.current_assets.cash_at_bank_and_in_hand")
     private Long previousAmount;
 
 }
--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/CurrentAssets.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/CurrentAssets.java
@@ -2,6 +2,7 @@ package uk.gov.companieshouse.web.accounts.model.smallfull;
 
 import lombok.Getter;
 import lombok.Setter;
+import uk.gov.companieshouse.web.accounts.validation.ValidationMapping;
 
 @Getter
 @Setter
@@ -10,7 +11,11 @@ public class CurrentAssets {
     private Stocks stocks;
     private Debtors debtors;
     private CashAtBankAndInHand cashAtBankAndInHand;
+
+    @ValidationMapping("$.current_period.balance_sheet.current_assets.total")
     private Long currentCurrentAssetsTotal;
+
+    @ValidationMapping("$.previous_period.balance_sheet.current_assets.total")
     private Long previousCurrentAssetsTotal;
 
 
