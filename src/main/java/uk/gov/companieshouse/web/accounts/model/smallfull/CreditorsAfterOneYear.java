a575141bdcca19568c53a5935df7d34679edf064
Marc Ransome <mransome@companieshouse.gov.uk>
2018-10-23 10:51:57 +0100

    Add model classes

---
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/CreditorsAfterOneYear.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.web.accounts.model.smallfull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreditorsAfterOneYear {
+
+    private Long currentAmount;
+
+    private Long previousAmount;
+}
---
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/TotalNetAssets.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.web.accounts.model.smallfull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TotalNetAssets {
+
+    private Long currentAmount;
+
+    private Long previousAmount;
+}
