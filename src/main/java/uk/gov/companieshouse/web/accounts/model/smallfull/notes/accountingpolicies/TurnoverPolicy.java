e0e400998929f5f5a1f17119f950c9391212818a
ch-mcabrera <mcabrerahernandez@companieshouse.gov.uk>
2018-11-15 14:05:40 +0000

    Adding turnover policy controller

--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/accountingpolicies/TurnoverPolicy.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/accountingpolicies/TurnoverPolicy.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class TurnoverPolicy {
-    @NotNull
+    @NotNull(message = "{turnoverPolicy.selectionNotMade}")
     private Boolean isIncludeTurnoverSelected;
 
     private String turnoverPolicyDetails;
--- src/main/java/uk/gov/companieshouse/web/accounts/service/smallfull/impl/TurnoverPolicyServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/service/smallfull/impl/TurnoverPolicyServiceImpl.java
@@ -42,11 +42,19 @@ public class TurnoverPolicyServiceImpl implements TurnoverPolicyService {
         AccountingPoliciesApi accountingPoliciesApi =
             accountingPoliciesService.getAccountingPoliciesApi(transactionId, companyAccountsId);
 
-        accountingPoliciesTransformer.setTurnoverPolicy(turnoverPolicy, accountingPoliciesApi);
+        //TODO remove when basis of preparation controller is there.
+        if (accountingPoliciesApi == null) {
+            accountingPoliciesApi = new AccountingPoliciesApi();
+            accountingPoliciesTransformer.setTurnoverPolicy(turnoverPolicy, accountingPoliciesApi);
 
-        return accountingPoliciesService
-            .updateAccountingPoliciesApi(transactionId, companyAccountsId,
-                accountingPoliciesApi);
+            return accountingPoliciesService.createAccountingPoliciesApi(transactionId, companyAccountsId, accountingPoliciesApi);
+
+        } else {
+            accountingPoliciesTransformer.setTurnoverPolicy(turnoverPolicy, accountingPoliciesApi);
+            return accountingPoliciesService
+                .updateAccountingPoliciesApi(transactionId, companyAccountsId,
+                    accountingPoliciesApi);
+        }
 
     }
 }
