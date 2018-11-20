9d6fab33daee5d09bef1d4840fa4f475c508a1e1
ch-mcabrera <mcabrerahernandez@companieshouse.gov.uk>
2018-11-20 11:38:38 +0000

    Adding validation for the text box - Adding messages to the messages.properties - Adding annotations to the TurnoverPolicy

--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/accountingpolicies/TurnoverPolicy.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/accountingpolicies/TurnoverPolicy.java
@@ -3,12 +3,32 @@ package uk.gov.companieshouse.web.accounts.model.smallfull.notes.accountingpolic
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import uk.gov.companieshouse.web.accounts.validation.ValidationMapping;
+import uk.gov.companieshouse.web.accounts.validation.ValidationModel;
 
 @Getter
 @Setter
+@ValidationModel
 public class TurnoverPolicy {
     @NotNull(message = "{turnoverPolicy.selectionNotMade}")
     private Boolean isIncludeTurnoverSelected;
 
+    @ValidationMapping("$.accounting_policies.turnover_policy")
     private String turnoverPolicyDetails;
+
+    public Boolean getIsIncludeTurnoverSelected() {
+        return isIncludeTurnoverSelected;
+    }
+
+    public void setIsIncludeTurnoverSelected(Boolean isIncludeTurnoverSelected) {
+        this.isIncludeTurnoverSelected = isIncludeTurnoverSelected;
+    }
+
+    public String getTurnoverPolicyDetails() {
+        return turnoverPolicyDetails;
+    }
+
+    public void setTurnoverPolicyDetails(String turnoverPolicyDetails) {
+        this.turnoverPolicyDetails = turnoverPolicyDetails;
+    }
 }
