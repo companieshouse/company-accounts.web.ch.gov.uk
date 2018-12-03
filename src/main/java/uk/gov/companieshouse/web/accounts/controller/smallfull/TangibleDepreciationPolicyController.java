c5aebd6eac3dbd25bc6b79bfa35be70c6afff592
sroberts2 <sroberts2@companieshouse.gov.uk>
2018-12-03 11:19:09 +0000

    changes in messages and whitespace

--- src/main/java/uk/gov/companieshouse/web/accounts/controller/smallfull/TangibleDepreciationPolicyController.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/controller/smallfull/TangibleDepreciationPolicyController.java
@@ -89,7 +89,6 @@ public class TangibleDepreciationPolicyController extends BaseController {
     }
 
     @Override
-
     protected String getTemplateName() {
         return "smallfull/tangibleDepreciationPolicy";
     }
