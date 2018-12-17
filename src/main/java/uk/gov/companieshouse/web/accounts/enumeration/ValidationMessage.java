357123ea431847d0fc50fd493645b374e6977396
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-12-17 14:17:56 +0000

    Correct api error returned when entering an invalid character

--- src/main/java/uk/gov/companieshouse/web/accounts/enumeration/ValidationMessage.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/enumeration/ValidationMessage.java
@@ -15,7 +15,7 @@ public enum ValidationMessage {
     INVALID_CHARACTER("invalid_character", "validation.character.invalid", true),
     INVALID_INPUT_LENGTH("invalid_input_length", "validation.length.invalidInputLength", false),
     INCORRECT_TOTAL("incorrect_total", "validation.total.invalid", true),
-    INVALID_CHARACTERS_ENTERED("invalid_characters_entered", "validation.characters.invalid", false),
+    INVALID_CHARACTERS_ENTERED("invalid_character", "validation.characters.invalid", false),
     MANDATORY_ELEMENT_MISSING("mandatory_element_missing", "validation.element.missing", false),
     DATE_CANNOT_BE_FUTURE("date_cannot_be_in_future", "validation.date.cannotBeFuture", true),
     DATE_INVALID("date_is_invalid", "validation.date.invalid", false),
