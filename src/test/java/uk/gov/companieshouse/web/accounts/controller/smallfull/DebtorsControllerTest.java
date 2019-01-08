7af07df8469e61c4bc4f1a364603b615e8dd03b6
caismith-ch <csmith2@companieshouse.gov.uk>
2019-01-08 16:27:22 +0000

    Complete Debtors controller tests - add tests for post functionality

--- src/test/java/uk/gov/companieshouse/web/accounts/controller/smallfull/DebtorsControllerTest.java
+++ src/test/java/uk/gov/companieshouse/web/accounts/controller/smallfull/DebtorsControllerTest.java
@@ -10,15 +10,23 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import uk.gov.companieshouse.web.accounts.exception.ServiceException;
 import uk.gov.companieshouse.web.accounts.model.smallfull.notes.Debtors;
 import uk.gov.companieshouse.web.accounts.service.smallfull.DebtorsService;
+import uk.gov.companieshouse.web.accounts.validation.ValidationError;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -41,10 +49,14 @@ public class DebtorsControllerTest {
 
     private static final String COMPANY_ACCOUNTS_ID = "companyAccountsId";
 
-    private static final String DEBTORS_PATH = "/company/" + COMPANY_NUMBER +
+    private static final String SMALL_FULL_PATH = "/company/" + COMPANY_NUMBER +
         "/transaction/" + TRANSACTION_ID +
         "/company-accounts/" + COMPANY_ACCOUNTS_ID +
-        "/small-full/debtors";
+        "/small-full";
+
+    private static final String DEBTORS_PATH = SMALL_FULL_PATH + "/debtors";
+
+    private static final String REVIEW_PATH = SMALL_FULL_PATH + "/review";
 
     private static final String DEBTORS_MODEL_ATTR = "debtors";
 
@@ -87,6 +99,63 @@ public class DebtorsControllerTest {
         this.mockMvc.perform(get(DEBTORS_PATH))
             .andExpect(status().isOk())
             .andExpect(view().name(ERROR_VIEW))
+            .andExpect(model().attributeExists(TEMPLATE_NAME_MODEL_ATTR));
+    }
+
+    @Test
+    @DisplayName("Post debtors success path")
+    void postRequestSuccess() throws Exception {
+
+        when(mockDebtorsService.submitDebtors(anyString(), anyString(), any(Debtors.class), anyString())).thenReturn(new ArrayList<>());
+
+        this.mockMvc.perform(post(DEBTORS_PATH))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(view().name(UrlBasedViewResolver.REDIRECT_URL_PREFIX + REVIEW_PATH));
+    }
+
+    @Test
+    @DisplayName("Post debtors failure path")
+    void postRequestFailure() throws Exception {
+
+        doThrow(ServiceException.class)
+            .when(mockDebtorsService).submitDebtors(anyString(), anyString(), any(Debtors.class), anyString());
+
+        this.mockMvc.perform(post(DEBTORS_PATH))
+            .andExpect(status().isOk())
+            .andExpect(view().name(ERROR_VIEW))
+            .andExpect(model().attributeExists(TEMPLATE_NAME_MODEL_ATTR));
+    }
+
+    @Test
+    @DisplayName("Post debtors failure path with API validation errors")
+    void postRequestFailureWithApiValidationErrors() throws Exception {
+
+        ValidationError validationError = new ValidationError();
+        validationError.setFieldPath("currentTradeDebtors");
+        validationError.setMessageKey("invalid_character");
+
+        List<ValidationError> errors = new ArrayList<>();
+        errors.add(validationError);
+
+        when(mockDebtorsService.submitDebtors(anyString(), anyString(), any(Debtors.class), anyString())).thenReturn(errors);
+
+        this.mockMvc.perform(post(DEBTORS_PATH))
+            .andExpect(status().isOk())
+            .andExpect(view().name(DEBTORS_VIEW));
+    }
+
+    @Test
+    @DisplayName("Post debtors with binding result errors")
+    void postRequestBindingResultErrors() throws Exception {
+
+        String beanElement = "currentTradeDebtors";
+        // Mock non-numeric input to trigger binding result errors
+        String invalidData = "test";
+
+        this.mockMvc.perform(post(DEBTORS_PATH)
+            .param(beanElement, invalidData))
+            .andExpect(status().isOk())
+            .andExpect(view().name(DEBTORS_VIEW))
             .andExpect(model().attributeExists(TEMPLATE_NAME_MODEL_ATTR));
     }
 }
