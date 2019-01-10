93829da026d63b917b9ea44240391464939d2ae2
leahtaylor <ltaylor@companieshouse.gov.uk>
2019-01-10 12:00:13 +0000

    Update review to match new debtors models

--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/Review.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/Review.java
@@ -2,13 +2,13 @@ package uk.gov.companieshouse.web.accounts.model.smallfull;
 
 import lombok.Getter;
 import lombok.Setter;
-import uk.gov.companieshouse.web.accounts.model.smallfull.notes.Debtors;
 import uk.gov.companieshouse.web.accounts.model.smallfull.notes.accountingpolicies.BasisOfPreparation;
 import uk.gov.companieshouse.web.accounts.model.smallfull.notes.accountingpolicies.IntangibleAmortisationPolicy;
 import uk.gov.companieshouse.web.accounts.model.smallfull.notes.accountingpolicies.TangibleDepreciationPolicy;
 import uk.gov.companieshouse.web.accounts.model.smallfull.notes.accountingpolicies.OtherAccountingPolicy;
 import uk.gov.companieshouse.web.accounts.model.smallfull.notes.accountingpolicies.TurnoverPolicy;
 import uk.gov.companieshouse.web.accounts.model.smallfull.notes.accountingpolicies.ValuationInformationPolicy;
+import uk.gov.companieshouse.web.accounts.model.smallfull.notes.debtors.Debtors;
 
 @Getter
 @Setter
