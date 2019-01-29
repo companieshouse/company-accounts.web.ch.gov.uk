cec651ac0a094916745d6a7f89fd98b7e9ae7484
leahtaylor <ltaylor@companieshouse.gov.uk>
2019-01-29 10:14:44 +0000

    Add POST and update functionality for creditors after one year

--- src/main/java/uk/gov/companieshouse/web/accounts/transformer/smallfull/impl/CreditorsAfterOneYearTransformerImpl.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/transformer/smallfull/impl/CreditorsAfterOneYearTransformerImpl.java
@@ -1,5 +1,7 @@
 package uk.gov.companieshouse.web.accounts.transformer.smallfull.impl;
 
+import java.util.Objects;
+import java.util.stream.Stream;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.model.accounts.smallfull.creditorsafteroneyear.CreditorsAfterOneYearApi;
 import uk.gov.companieshouse.api.model.accounts.smallfull.creditorsafteroneyear.CurrentPeriod;
@@ -43,6 +45,25 @@ public class CreditorsAfterOneYearTransformerImpl implements CreditorsAfterOneYe
         creditorsAfterOneYear.setTotal(total);
 
         return creditorsAfterOneYear;
+    }
+
+
+    @Override
+    public CreditorsAfterOneYearApi getCreditorsAfterOneYearApi(CreditorsAfterOneYear creditorsAfterOneYear) {
+
+        CreditorsAfterOneYearApi creditorsAfterOneYearApi = new CreditorsAfterOneYearApi();
+
+        if (creditorsAfterOneYear.getTotal() != null) {
+            if (creditorsAfterOneYear.getTotal().getCurrentTotal() != null) {
+                setCurrentPeriodOnApiModel(creditorsAfterOneYear, creditorsAfterOneYearApi);
+            }
+
+            if (creditorsAfterOneYear.getTotal().getPreviousTotal() != null) {
+                setPreviousPeriodOnApiModel(creditorsAfterOneYear, creditorsAfterOneYearApi);
+            }
+        }
+
+        return creditorsAfterOneYearApi;
     }
 
     private void populateCurrentPeriodForWeb(CreditorsAfterOneYearApi creditorsAfterOneYearApi,
@@ -89,8 +110,85 @@ public class CreditorsAfterOneYearTransformerImpl implements CreditorsAfterOneYe
         }
     }
 
-    @Override
-    public CreditorsAfterOneYearApi getCreditorsAfterOneYearApi(CreditorsAfterOneYear creditorsAfterOneYear) {
-        return null;
+    private void setCurrentPeriodOnApiModel(CreditorsAfterOneYear creditorsAfterOneYear,
+            CreditorsAfterOneYearApi creditorsAfterOneYearApi) {
+        CurrentPeriod currentPeriod = new CurrentPeriod();
+
+        if (creditorsAfterOneYear.getDetails() != null
+                && creditorsAfterOneYear.getDetails().equals("")) {
+            currentPeriod.setDetails(null);
+        } else {
+            currentPeriod.setDetails(creditorsAfterOneYear.getDetails());
+        }
+
+        if (creditorsAfterOneYear.getBankLoansAndOverdrafts() != null
+                && creditorsAfterOneYear.getBankLoansAndOverdrafts().getCurrentBankLoansAndOverdrafts() != null) {
+            currentPeriod.setBankLoansAndOverdrafts(creditorsAfterOneYear.getBankLoansAndOverdrafts()
+                    .getCurrentBankLoansAndOverdrafts());
+        }
+
+        if (creditorsAfterOneYear.getFinanceLeasesAndHirePurchaseContracts() != null
+                && creditorsAfterOneYear.getFinanceLeasesAndHirePurchaseContracts()
+                .getCurrentFinanceLeasesAndHirePurchaseContracts() != null) {
+            currentPeriod.setFinanceLeasesAndHirePurchaseContracts(creditorsAfterOneYear
+                    .getFinanceLeasesAndHirePurchaseContracts()
+                    .getCurrentFinanceLeasesAndHirePurchaseContracts());
+        }
+
+        if (creditorsAfterOneYear.getOtherCreditors() != null
+                && creditorsAfterOneYear.getOtherCreditors().getCurrentOtherCreditors() != null) {
+            currentPeriod.setOtherCreditors(creditorsAfterOneYear.getOtherCreditors()
+                    .getCurrentOtherCreditors());
+        }
+
+        if (creditorsAfterOneYear.getTotal() != null
+                && creditorsAfterOneYear.getTotal().getCurrentTotal() != null) {
+            currentPeriod.setTotal(creditorsAfterOneYear.getTotal().getCurrentTotal());
+        }
+
+        creditorsAfterOneYearApi.setCurrentPeriod(currentPeriod);
     }
+
+    private void setPreviousPeriodOnApiModel(CreditorsAfterOneYear creditorsAfterOneYear,
+            CreditorsAfterOneYearApi creditorsAfterOneYearApi) {
+        PreviousPeriod previousPeriod = new PreviousPeriod();
+
+        if (creditorsAfterOneYear.getBankLoansAndOverdrafts() != null
+                && creditorsAfterOneYear.getBankLoansAndOverdrafts().getPreviousBankLoansAndOverdrafts() != null) {
+            previousPeriod.setBankLoansAndOverdrafts(creditorsAfterOneYear.getBankLoansAndOverdrafts()
+                    .getPreviousBankLoansAndOverdrafts());
+        }
+
+        if (creditorsAfterOneYear.getFinanceLeasesAndHirePurchaseContracts() != null
+                && creditorsAfterOneYear.getFinanceLeasesAndHirePurchaseContracts()
+                .getPreviousFinanceLeasesAndHirePurchaseContracts() != null) {
+            previousPeriod.setFinanceLeasesAndHirePurchaseContracts(creditorsAfterOneYear
+                    .getFinanceLeasesAndHirePurchaseContracts()
+                    .getPreviousFinanceLeasesAndHirePurchaseContracts());
+        }
+
+        if (creditorsAfterOneYear.getOtherCreditors() != null
+                && creditorsAfterOneYear.getOtherCreditors().getPreviousOtherCreditors() != null) {
+            previousPeriod.setOtherCreditors(creditorsAfterOneYear.getOtherCreditors()
+                    .getPreviousOtherCreditors());
+        }
+
+        if (creditorsAfterOneYear.getTotal() != null
+                && creditorsAfterOneYear.getTotal().getPreviousTotal() != null) {
+            previousPeriod.setTotal(creditorsAfterOneYear.getTotal().getPreviousTotal());
+        }
+
+        if (isPreviousPeriodPopulated(previousPeriod)) {
+            creditorsAfterOneYearApi.setPreviousPeriod(previousPeriod);
+        }
+    }
+
+    private boolean isPreviousPeriodPopulated(PreviousPeriod period) {
+        return Stream.of(
+                period.getBankLoansAndOverdrafts(),
+                period.getFinanceLeasesAndHirePurchaseContracts(),
+                period.getOtherCreditors(),
+           period.getTotal()).anyMatch(Objects ::nonNull);
+    }
+
 }
