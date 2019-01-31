6296c76c25129ad22d99ee9596448d9d98355b87
bsaunders95 <bsaunders@companieshouse.gov.uk>
2019-01-31 15:45:47 +0000

    Remove repeated import

--- src/main/java/uk/gov/companieshouse/web/accounts/util/ValidationContext.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/util/ValidationContext.java
@@ -1,6 +1,5 @@
 package uk.gov.companieshouse.web.accounts.util;
 
-import java.time.LocalDate;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
