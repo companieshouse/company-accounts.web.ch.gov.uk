0ff2ec2658c968fad33fd6d631b7f45ff6d66114
Shaun Mathias <smathias@companieshouse.gov.uk>
2019-02-07 15:03:09 +0000

    Added validationMapping for web validation on stocks Updated message.properties to include mapping Updated stocks.html to include javascript

--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/stocks/Stocks.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/stocks/Stocks.java
@@ -2,12 +2,15 @@ package uk.gov.companieshouse.web.accounts.model.smallfull.notes.stocks;
 
 import lombok.Getter;
 import lombok.Setter;
+import uk.gov.companieshouse.web.accounts.validation.ValidationMapping;
 
 @Getter
 @Setter
 public class Stocks {
 
+    @ValidationMapping("$.stocks.current_period.stocks")
     private Long currentStocks;
 
+    @ValidationMapping("$.stocks.previous_period.stocks")
     private Long previousStocks;
 }
--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/stocks/Total.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/stocks/Total.java
@@ -2,12 +2,15 @@ package uk.gov.companieshouse.web.accounts.model.smallfull.notes.stocks;
 
 import lombok.Getter;
 import lombok.Setter;
+import uk.gov.companieshouse.web.accounts.validation.ValidationMapping;
 
 @Getter
 @Setter
 public class Total {
 
+    @ValidationMapping("$.stocks.current_period.total")
     private Long currentTotal;
 
+    @ValidationMapping("$.stocks.previous_period.total")
     private Long previousTotal;
 }
