ac015b1e2fffba8e16373c36ad0152630a921985
Marc Ransome <mransome@companieshouse.gov.uk>
2019-02-08 14:27:44 +0000

    Ensure consistent note validation for multiple scenarios

--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/stocks/Total.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/stocks/Total.java
@@ -2,12 +2,15 @@ package uk.gov.companieshouse.web.accounts.model.smallfull.notes.stocks;
 
 import lombok.Getter;
 import lombok.Setter;
+import uk.gov.companieshouse.web.accounts.validation.ValidationPeriodMapping;
 
 @Getter
 @Setter
 public class Total {
 
+    @ValidationPeriodMapping("$.stocks.current_period")
     private Long currentTotal;
 
+    @ValidationPeriodMapping("$.stocks.previous_period")
     private Long previousTotal;
 }
