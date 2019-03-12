93f2bc19242aa25534f54d3814b0c75b607fd9e5
Shaun Mathias <smathias@companieshouse.gov.uk>
2019-03-12 12:24:58 +0000

    Added CurrentAssetsInvestments details to review screen Added CurrentAssetsInvestment to the review model

--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/currentassetsinvestments/CurrentAssetsInvestments.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/currentassetsinvestments/CurrentAssetsInvestments.java
@@ -5,11 +5,14 @@ import lombok.Setter;
 import uk.gov.companieshouse.web.accounts.validation.ValidationMapping;
 import uk.gov.companieshouse.web.accounts.validation.ValidationModel;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @ValidationModel
 public class CurrentAssetsInvestments {
 
+    @NotBlank(message = "{currentAssetsInvestments.details.missing}")
     @ValidationMapping("$.current_assets_investments.details")
     private String details;
 }
