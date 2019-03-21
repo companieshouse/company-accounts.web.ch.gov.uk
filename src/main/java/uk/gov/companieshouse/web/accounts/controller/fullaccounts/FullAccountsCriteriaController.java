5bfd48517392d5fcaa8dab7112d4e34abe0a2625
ethatcher83 <ethatcher@companieshouse.gov.uk>
2019-03-21 11:45:29 +0000

    Added redirect to CHS home

--- src/main/java/uk/gov/companieshouse/web/accounts/controller/fullaccounts/FullAccountsCriteriaController.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/controller/fullaccounts/FullAccountsCriteriaController.java
@@ -1,6 +1,8 @@
 package uk.gov.companieshouse.web.accounts.controller.fullaccounts;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,21 +13,24 @@ import uk.gov.companieshouse.web.accounts.controller.BaseController;
 @RequestMapping("/accounts/full-accounts-criteria")
 public class FullAccountsCriteriaController extends BaseController{
 
+    @Value("${chs.url}")
+    private String chsUrl;
+
     @GetMapping
-    public String getCriteria(){
+    public String getCriteria(Model model){
+        model.addAttribute("hideUserBar" , true);
         return getTemplateName();
+    }
+
+    @PostMapping
+    public String postFullAccountsCriteria(){
+        return UrlBasedViewResolver.REDIRECT_URL_PREFIX + chsUrl;
     }
 
     @Override
     protected String getTemplateName() {
         return "fullaccounts/criteria";
     }
-
-    @PostMapping
-    public String postFullAccountsCriteria(){
-        return UrlBasedViewResolver.REDIRECT_URL_PREFIX + "/";
-    }
-
 
 }
 
