f05b8796e04a02f6ab816dfc9324f9b4c2457e0b
caismith-ch <csmith2@companieshouse.gov.uk>
2019-03-27 15:34:35 +0000

    Fix failing tests due to missing navigator service mock

--- src/test/java/uk/gov/companieshouse/web/accounts/controller/govuk/smallfull/GovukFullAccountsCriteriaControllerTest.java
+++ src/test/java/uk/gov/companieshouse/web/accounts/controller/govuk/smallfull/GovukFullAccountsCriteriaControllerTest.java
@@ -6,13 +6,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import uk.gov.companieshouse.web.accounts.service.navigation.NavigatorService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,6 +28,9 @@ public class GovukFullAccountsCriteriaControllerTest {
 
     private MockMvc mockMvc;
 
+    @Mock
+    private NavigatorService navigatorService;
+
     @InjectMocks
     private GovukFullAccountsCriteriaController controller;
 
@@ -31,12 +38,12 @@ public class GovukFullAccountsCriteriaControllerTest {
     private static final String PATH = "/accounts/full-accounts-criteria";
     private static final String MODEL_ATTR = "hideUserBar";
     private static final String TEMPLATE_NAME_MODEL_ATTR = "templateName";
-    private static final String CHS_HOME_PATH = UrlBasedViewResolver.REDIRECT_URL_PREFIX +
-        "/";
+    private static final String BACK_PAGE_MODEL_ATTR = "backButton";
+    private static final String CHS_HOME_PATH = UrlBasedViewResolver.REDIRECT_URL_PREFIX + "/";
+    private static final String MOCK_CONTROLLER_PATH = UrlBasedViewResolver.REDIRECT_URL_PREFIX + "mockControllerPath";
 
     @BeforeEach
     private void setup() {
-
         this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
@@ -44,10 +51,13 @@ public class GovukFullAccountsCriteriaControllerTest {
     @DisplayName("Get full accounts criteria view success path")
     void getRequestSuccess() throws Exception {
 
+        when(navigatorService.getPreviousControllerPath(any(), any())).thenReturn(MOCK_CONTROLLER_PATH);
+
         this.mockMvc.perform(get(PATH))
             .andExpect(status().isOk())
             .andExpect(view().name(VIEW))
             .andExpect(model().attributeExists(MODEL_ATTR))
+            .andExpect(model().attributeExists(BACK_PAGE_MODEL_ATTR))
             .andExpect(model().attributeExists(TEMPLATE_NAME_MODEL_ATTR));
     }
 
