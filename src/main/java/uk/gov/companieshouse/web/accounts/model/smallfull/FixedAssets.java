00ed1c81879824e95d5396a8f7830dffac930af6
ethatcher83 <ethatcher@companieshouse.gov.uk>
2019-04-04 13:00:00 +0100

    Add fix investments to balance sheet and update transformer

--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/FixedAssets.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/FixedAssets.java
@@ -9,11 +9,13 @@ import uk.gov.companieshouse.web.accounts.validation.ValidationMapping;
 public class FixedAssets {
 
     private TangibleAssets tangibleAssets;
+    private FixedInvestments investments;
 
     @ValidationMapping("$.previous_period.balance_sheet.fixed_assets.total")
     private Long previousTotal;
 
     @ValidationMapping("$.current_period.balance_sheet.fixed_assets.total")
     private Long currentTotal;
+
 
 }
