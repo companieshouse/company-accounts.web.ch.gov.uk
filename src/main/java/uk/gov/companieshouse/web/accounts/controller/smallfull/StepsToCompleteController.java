9b0a57e977507315fef2fe621419f16a44eafc2d
bsaunders95 <bsaunders@companieshouse.gov.uk>
2019-04-05 16:14:22 +0100

    Change body of company accounts POST request to be empty in line with API changes; accounting period dates are now derived from the company profile associated with the transaction used to create the company accounts

--- src/main/java/uk/gov/companieshouse/web/accounts/controller/smallfull/StepsToCompleteController.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/controller/smallfull/StepsToCompleteController.java
@@ -1,6 +1,5 @@
 package uk.gov.companieshouse.web.accounts.controller.smallfull;
 
-import java.time.LocalDate;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,12 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
 import uk.gov.companieshouse.web.accounts.annotation.NextController;
 import uk.gov.companieshouse.web.accounts.annotation.PreviousController;
 import uk.gov.companieshouse.web.accounts.controller.BaseController;
 import uk.gov.companieshouse.web.accounts.exception.ServiceException;
-import uk.gov.companieshouse.web.accounts.service.company.CompanyService;
 import uk.gov.companieshouse.web.accounts.service.companyaccounts.CompanyAccountsService;
 import uk.gov.companieshouse.web.accounts.service.smallfull.SmallFullService;
 import uk.gov.companieshouse.web.accounts.service.smallfull.StatementsService;
@@ -28,9 +25,6 @@ public class StepsToCompleteController extends BaseController {
 
     @Autowired
     private TransactionService transactionService;
-
-    @Autowired
-    private CompanyService companyService;
 
     @Autowired
     private SmallFullService smallFullService;
@@ -62,10 +56,7 @@ public class StepsToCompleteController extends BaseController {
         try {
             String transactionId = transactionService.createTransaction(companyNumber);
 
-            CompanyProfileApi companyProfile = companyService.getCompanyProfile(companyNumber);
-            LocalDate periodEndOn = companyProfile.getAccounts().getNextAccounts().getPeriodEndOn();
-
-            String companyAccountsId = companyAccountsService.createCompanyAccounts(transactionId, periodEndOn);
+            String companyAccountsId = companyAccountsService.createCompanyAccounts(transactionId);
 
             smallFullService.createSmallFullAccounts(transactionId, companyAccountsId);
 
