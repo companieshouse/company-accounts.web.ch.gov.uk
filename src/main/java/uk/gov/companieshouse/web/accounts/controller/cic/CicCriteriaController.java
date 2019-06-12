2e9fac670ca1ff8a067dcbf3631b33f60bc647d9
amoriartyCH <amoriarty@companieshouse.gov.uk>
2019-06-12 11:30:47 +0100

    Implemented Cic Criteria page and unit tests

--- src/main/java/uk/gov/companieshouse/web/accounts/controller/cic/CicCriteriaController.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/controller/cic/CicCriteriaController.java
@@ -2,11 +2,18 @@ package uk.gov.companieshouse.web.accounts.controller.cic;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import uk.gov.companieshouse.web.accounts.annotation.NextController;
 import uk.gov.companieshouse.web.accounts.controller.BaseController;
-import uk.gov.companieshouse.web.accounts.model.smallfull.Criteria;
+import uk.gov.companieshouse.web.accounts.model.cic.CicCriteria;
+
+import javax.validation.Valid;
 
 @Controller
 @NextController(CicCriteriaController.class)
@@ -21,8 +28,24 @@ public class CicCriteriaController extends BaseController {
     @GetMapping
     public String getCicCriteria(Model model) {
 
-        model.addAttribute("criteria", new Criteria());
-        addBackPageAttributeToModel(model);
+        model.addAttribute("criteria", new CicCriteria());
+
+        return getTemplateName();
+    }
+
+    @PostMapping
+    public String postCicCriteria(@ModelAttribute("criteria") @Valid CicCriteria criteria,
+                                  BindingResult bindingResult, Model model, RedirectAttributes attributes) {
+
+        if (bindingResult.hasErrors()) {
+            return getTemplateName();
+        }
+
+        if(criteria.getIsCriteriaMet()) {
+
+            attributes.addAttribute("forward", "/accounts/company/{companyNumber}/cic/details");
+            return UrlBasedViewResolver.REDIRECT_URL_PREFIX + "/company-lookup/search";
+        }
 
         return getTemplateName();
     }
