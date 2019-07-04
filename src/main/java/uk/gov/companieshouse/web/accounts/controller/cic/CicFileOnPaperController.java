0bf3e9011e5f0a2454b84ef32f7eb64634d140a6
amoriartyCH <amoriarty@companieshouse.gov.uk>
2019-07-04 08:26:48 +0100

    Removed unused imports

--- src/main/java/uk/gov/companieshouse/web/accounts/controller/cic/CicFileOnPaperController.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/controller/cic/CicFileOnPaperController.java
@@ -3,14 +3,10 @@ package uk.gov.companieshouse.web.accounts.controller.cic;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.util.UriTemplate;
 import uk.gov.companieshouse.web.accounts.annotation.PreviousController;
 import uk.gov.companieshouse.web.accounts.controller.BaseController;
-
-import java.util.Optional;
 
 @Controller
 @RequestMapping({"/accounts/cic/cics-file-paper", "/accounts/cic/{companyNumber}/cics-file-paper"})
