91ce221c6d2ed91570478adc32a3928559c33005
Jamie87 <jwillison@companieshouse.gov.uk>
2019-09-03 16:35:38 +0100

    Remove willRender

--- src/main/java/uk/gov/companieshouse/web/accounts/controller/smallfull/IntangibleAssetsNoteController.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/controller/smallfull/IntangibleAssetsNoteController.java
@@ -16,8 +16,6 @@ import uk.gov.companieshouse.web.accounts.controller.BaseController;
 import uk.gov.companieshouse.web.accounts.controller.ConditionalController;
 import uk.gov.companieshouse.web.accounts.exception.ServiceException;
 
-import uk.gov.companieshouse.web.accounts.model.smallfull.BalanceSheet;
-import uk.gov.companieshouse.web.accounts.model.smallfull.FixedAssets;
 import uk.gov.companieshouse.web.accounts.model.smallfull.notes.intangible.IntangibleAssets;
 import uk.gov.companieshouse.web.accounts.service.smallfull.BalanceSheetService;
 import uk.gov.companieshouse.web.accounts.service.smallfull.IntangibleAssetsNoteService;
@@ -26,7 +24,6 @@ import uk.gov.companieshouse.web.accounts.validation.ValidationError;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @NextController(TangibleAssetsNoteController.class)
@@ -106,15 +103,6 @@ public class IntangibleAssetsNoteController extends BaseController implements Co
 
     @Override
     public boolean willRender(String companyNumber, String transactionId, String companyAccountsId) throws ServiceException {
-
-        BalanceSheet balanceSheet =
-                balanceSheetService.getBalanceSheet(
-                        transactionId, companyAccountsId, companyNumber);
-
-        return hasIntangibleAssets(balanceSheet);
-    }
-
-    private boolean hasIntangibleAssets(BalanceSheet balanceSheet) {
 
         return false;
     }
