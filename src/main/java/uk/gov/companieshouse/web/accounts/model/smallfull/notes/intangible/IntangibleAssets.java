3a0a10da8e4836c772c6531f09c9dd6e0a101ab2
Faisal Amin <famin@companieshouse.gov.uk>
2019-09-04 18:18:09 +0100

    Added first year filer web validation code

--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/intangible/IntangibleAssets.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/intangible/IntangibleAssets.java
@@ -2,9 +2,11 @@ package uk.gov.companieshouse.web.accounts.model.smallfull.notes.intangible;
 
 
 import uk.gov.companieshouse.web.accounts.model.smallfull.notes.intangible.cost.IntangibleAssetsCost;
+import uk.gov.companieshouse.web.accounts.validation.ValidationModel;
 
 import java.time.LocalDate;
 
+@ValidationModel
 public class IntangibleAssets {
 
     private IntangibleAssetsCost cost;
