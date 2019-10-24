79128b415f6cc5994ae3cd996c0b98cf28bacadc
bsaunders95 <bsaunders@companieshouse.gov.uk>
2019-10-24 19:38:39 +0100

    Refactor null check for simplicity

--- src/main/java/uk/gov/companieshouse/web/accounts/service/company/impl/CompanyServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/service/company/impl/CompanyServiceImpl.java
@@ -69,7 +69,7 @@ public class CompanyServiceImpl implements CompanyService {
                 .map(CompanyProfileApi::getAccounts)
                 .map(CompanyAccountApi::getLastAccounts)
                 .map(LastAccountsApi::getPeriodEndOn)
-                .orElse(null) != null;
+                .isPresent();
     }
 
     @Override
