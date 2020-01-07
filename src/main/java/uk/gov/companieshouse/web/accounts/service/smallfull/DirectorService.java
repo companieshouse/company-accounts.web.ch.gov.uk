2ff6774cd2cfe59be5b9a6e3d857d12df0c663be
bsaunders95 <bsaunders@companieshouse.gov.uk>
2020-01-07 15:32:17 +0000

    Remove unused code

--- src/main/java/uk/gov/companieshouse/web/accounts/service/smallfull/DirectorService.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/service/smallfull/DirectorService.java
@@ -2,10 +2,7 @@ package uk.gov.companieshouse.web.accounts.service.smallfull;
 
 import java.util.List;
 
-import uk.gov.companieshouse.api.error.ApiErrorResponseException;
-import uk.gov.companieshouse.api.handler.exception.URIValidationException;
 import uk.gov.companieshouse.web.accounts.exception.ServiceException;
-import uk.gov.companieshouse.web.accounts.model.directorsreport.AddOrRemoveDirectors;
 import uk.gov.companieshouse.web.accounts.model.directorsreport.Director;
 import uk.gov.companieshouse.web.accounts.model.directorsreport.DirectorToAdd;
 import uk.gov.companieshouse.web.accounts.validation.ValidationError;
@@ -17,7 +14,4 @@ public interface DirectorService {
     List<ValidationError> createDirector(String transactionId, String companyAccountsId, DirectorToAdd directorToAdd) throws ServiceException;
 
     void deleteDirector(String transactionId, String companyAccountsId, String directorId) throws ServiceException;
-
-    List<ValidationError> submitDirectorsReport(String transactionId, String companyAccountsId, String companyNumber, AddOrRemoveDirectors addOrRemoveDirectors)
-            throws ServiceException;
 }
