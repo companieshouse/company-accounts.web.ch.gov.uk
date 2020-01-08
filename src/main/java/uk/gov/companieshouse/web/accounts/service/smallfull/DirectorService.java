7f01b35c262a83087ce7d20702c2cb3340477d4f
bsaunders95 <bsaunders@companieshouse.gov.uk>
2020-01-08 16:31:43 +0000

    Initial implementation

--- src/main/java/uk/gov/companieshouse/web/accounts/service/smallfull/DirectorService.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/service/smallfull/DirectorService.java
@@ -3,6 +3,7 @@ package uk.gov.companieshouse.web.accounts.service.smallfull;
 import java.util.List;
 
 import uk.gov.companieshouse.web.accounts.exception.ServiceException;
+import uk.gov.companieshouse.web.accounts.model.directorsreport.AddOrRemoveDirectors;
 import uk.gov.companieshouse.web.accounts.model.directorsreport.Director;
 import uk.gov.companieshouse.web.accounts.model.directorsreport.DirectorToAdd;
 import uk.gov.companieshouse.web.accounts.validation.ValidationError;
@@ -14,4 +15,6 @@ public interface DirectorService {
     List<ValidationError> createDirector(String transactionId, String companyAccountsId, DirectorToAdd directorToAdd) throws ServiceException;
 
     void deleteDirector(String transactionId, String companyAccountsId, String directorId) throws ServiceException;
+
+    List<ValidationError> submitAddOrRemoveDirectors(AddOrRemoveDirectors addOrRemoveDirectors);
 }
--- src/main/java/uk/gov/companieshouse/web/accounts/validation/smallfull/DirectorValidator.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/validation/smallfull/DirectorValidator.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.web.accounts.model.directorsreport.AddOrRemoveDirectors;
 import uk.gov.companieshouse.web.accounts.model.directorsreport.DirectorToAdd;
 import uk.gov.companieshouse.web.accounts.validation.ValidationError;
 
 @Component
 public class DirectorValidator {
+
+    private static final String DIRECTOR_TO_ADD = "directorToAdd";
+    private static final String DIRECTOR_MUST_BE_ADDED = "validation.directorToAdd.submissionRequired";
+    private static final String AT_LEAST_ONE_DIRECTOR_REQUIRED = "validation.addOrRemoveDirectors.oneRequired";
 
     private static final String NAME = "directorToAdd.name";
     private static final String NAME_NOT_PRESENT = "validation.element.missing.director.name";
@@ -44,6 +49,30 @@ public class DirectorValidator {
             ValidationError error = new ValidationError();
             error.setFieldPath(DID_DIRECTOR_RESIGN);
             error.setMessageKey(RESIGNATION_NOT_SELECTED);
+            validationErrors.add(error);
+        }
+
+        return validationErrors;
+    }
+
+    public List<ValidationError> validateSubmitAddOrRemoveDirectors(AddOrRemoveDirectors addOrRemoveDirectors) {
+
+        List<ValidationError> validationErrors = new ArrayList<>();
+
+        if (StringUtils.isNotBlank(addOrRemoveDirectors.getDirectorToAdd().getName()) ||
+                addOrRemoveDirectors.getDirectorToAdd().getWasDirectorAppointedDuringPeriod() != null ||
+                addOrRemoveDirectors.getDirectorToAdd().getDidDirectorResignDuringPeriod() != null) {
+
+            ValidationError error = new ValidationError();
+            error.setFieldPath(DIRECTOR_TO_ADD);
+            error.setMessageKey(DIRECTOR_MUST_BE_ADDED);
+            validationErrors.add(error);
+
+        } else if (addOrRemoveDirectors.getExistingDirectors() == null) {
+
+            ValidationError error = new ValidationError();
+            error.setFieldPath(DIRECTOR_TO_ADD);
+            error.setMessageKey(AT_LEAST_ONE_DIRECTOR_REQUIRED);
             validationErrors.add(error);
         }
 
