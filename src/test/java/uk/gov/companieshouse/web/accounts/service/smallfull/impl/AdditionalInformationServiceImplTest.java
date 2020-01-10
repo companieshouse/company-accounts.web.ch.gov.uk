a4ba2e6e6c6549bb77fa270059ad849506c5a40c
bsaunders95 <bsaunders@companieshouse.gov.uk>
2020-01-10 19:12:41 +0000

    Add supporting statements service unit tests

---
+++ src/test/java/uk/gov/companieshouse/web/accounts/service/smallfull/impl/AdditionalInformationServiceImplTest.java
@@ -0,0 +1,100 @@
+package uk.gov.companieshouse.web.accounts.service.smallfull.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.companieshouse.api.model.accounts.directorsreport.StatementsApi;
+import uk.gov.companieshouse.web.accounts.exception.ServiceException;
+import uk.gov.companieshouse.web.accounts.model.directorsreport.AdditionalInformation;
+import uk.gov.companieshouse.web.accounts.service.smallfull.AdditionalInformationService;
+import uk.gov.companieshouse.web.accounts.service.smallfull.DirectorsReportStatementsService;
+import uk.gov.companieshouse.web.accounts.transformer.smallfull.directorsreport.DirectorsReportStatementsTransformer;
+import uk.gov.companieshouse.web.accounts.validation.ValidationError;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AdditionalInformationServiceImplTest {
+
+    @Mock
+    private DirectorsReportStatementsService directorsReportStatementsService;
+
+    @Mock
+    private DirectorsReportStatementsTransformer directorsReportStatementsTransformer;
+
+    @InjectMocks
+    private AdditionalInformationService additionalInformationService = new AdditionalInformationServiceImpl();
+
+    @Mock
+    private AdditionalInformation additionalInformation;
+
+    @Mock
+    private StatementsApi statementsApi;
+
+    @Mock
+    private List<ValidationError> validationErrors;
+
+    private static final String TRANSACTION_ID = "transactionId";
+
+    private static final String COMPANY_ACCOUNTS_ID = "companyAccountsId";
+
+    @Test
+    @DisplayName("Get additional information")
+    void getAdditionalInformation() throws ServiceException {
+
+        when(directorsReportStatementsService.getDirectorsReportStatements(TRANSACTION_ID, COMPANY_ACCOUNTS_ID))
+                .thenReturn(statementsApi);
+
+        when(directorsReportStatementsTransformer.getAdditionalInformation(statementsApi))
+                .thenReturn(additionalInformation);
+
+        AdditionalInformation returned =
+                additionalInformationService.getAdditionalInformation(TRANSACTION_ID, COMPANY_ACCOUNTS_ID);
+
+        assertEquals(additionalInformation, returned);
+    }
+
+    @Test
+    @DisplayName("Submit additional information - create")
+    void submitAdditionalInformationCreate() throws ServiceException {
+
+        when(directorsReportStatementsService.getDirectorsReportStatements(TRANSACTION_ID, COMPANY_ACCOUNTS_ID))
+                .thenReturn(null);
+
+        when(directorsReportStatementsService.createDirectorsReportStatements(eq(TRANSACTION_ID), eq(COMPANY_ACCOUNTS_ID), any(StatementsApi.class)))
+                .thenReturn(validationErrors);
+
+        List<ValidationError> returned = additionalInformationService.submitAdditionalInformation(TRANSACTION_ID, COMPANY_ACCOUNTS_ID, additionalInformation);
+
+        assertEquals(validationErrors, returned);
+
+        verify(directorsReportStatementsTransformer).setAdditionalInformation(any(StatementsApi.class), eq(additionalInformation));
+    }
+
+    @Test
+    @DisplayName("Submit additional information - update")
+    void submitAdditionalInformationUpdate() throws ServiceException {
+
+        when(directorsReportStatementsService.getDirectorsReportStatements(TRANSACTION_ID, COMPANY_ACCOUNTS_ID))
+                .thenReturn(statementsApi);
+
+        when(directorsReportStatementsService.updateDirectorsReportStatements(TRANSACTION_ID, COMPANY_ACCOUNTS_ID, statementsApi))
+                .thenReturn(validationErrors);
+
+        List<ValidationError> returned = additionalInformationService.submitAdditionalInformation(TRANSACTION_ID, COMPANY_ACCOUNTS_ID, additionalInformation);
+
+        assertEquals(validationErrors, returned);
+
+        verify(directorsReportStatementsTransformer).setAdditionalInformation(statementsApi, additionalInformation);
+    }
+}
---
+++ src/test/java/uk/gov/companieshouse/web/accounts/service/smallfull/impl/PrincipalActivitiesServiceImplTest.java
@@ -0,0 +1,100 @@
+package uk.gov.companieshouse.web.accounts.service.smallfull.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.companieshouse.api.model.accounts.directorsreport.StatementsApi;
+import uk.gov.companieshouse.web.accounts.exception.ServiceException;
+import uk.gov.companieshouse.web.accounts.model.directorsreport.PrincipalActivities;
+import uk.gov.companieshouse.web.accounts.service.smallfull.PrincipalActivitiesService;
+import uk.gov.companieshouse.web.accounts.service.smallfull.DirectorsReportStatementsService;
+import uk.gov.companieshouse.web.accounts.transformer.smallfull.directorsreport.DirectorsReportStatementsTransformer;
+import uk.gov.companieshouse.web.accounts.validation.ValidationError;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PrincipalActivitiesServiceImplTest {
+
+    @Mock
+    private DirectorsReportStatementsService directorsReportStatementsService;
+
+    @Mock
+    private DirectorsReportStatementsTransformer directorsReportStatementsTransformer;
+
+    @InjectMocks
+    private PrincipalActivitiesService principalActivitiesService = new PrincipalActivitiesServiceImpl();
+
+    @Mock
+    private PrincipalActivities principalActivities;
+
+    @Mock
+    private StatementsApi statementsApi;
+
+    @Mock
+    private List<ValidationError> validationErrors;
+
+    private static final String TRANSACTION_ID = "transactionId";
+
+    private static final String COMPANY_ACCOUNTS_ID = "companyAccountsId";
+
+    @Test
+    @DisplayName("Get principal activities")
+    void getPrincipalActivities() throws ServiceException {
+
+        when(directorsReportStatementsService.getDirectorsReportStatements(TRANSACTION_ID, COMPANY_ACCOUNTS_ID))
+                .thenReturn(statementsApi);
+
+        when(directorsReportStatementsTransformer.getPrincipalActivities(statementsApi))
+                .thenReturn(principalActivities);
+
+        PrincipalActivities returned =
+                principalActivitiesService.getPrincipalActivities(TRANSACTION_ID, COMPANY_ACCOUNTS_ID);
+
+        assertEquals(principalActivities, returned);
+    }
+
+    @Test
+    @DisplayName("Submit principal activities - create")
+    void submitPrincipalActivitiesCreate() throws ServiceException {
+
+        when(directorsReportStatementsService.getDirectorsReportStatements(TRANSACTION_ID, COMPANY_ACCOUNTS_ID))
+                .thenReturn(null);
+
+        when(directorsReportStatementsService.createDirectorsReportStatements(eq(TRANSACTION_ID), eq(COMPANY_ACCOUNTS_ID), any(StatementsApi.class)))
+                .thenReturn(validationErrors);
+
+        List<ValidationError> returned = principalActivitiesService.submitPrincipalActivities(TRANSACTION_ID, COMPANY_ACCOUNTS_ID, principalActivities);
+
+        assertEquals(validationErrors, returned);
+
+        verify(directorsReportStatementsTransformer).setPrincipalActivities(any(StatementsApi.class), eq(principalActivities));
+    }
+
+    @Test
+    @DisplayName("Submit principal activities - update")
+    void submitPrincipalActivitiesUpdate() throws ServiceException {
+
+        when(directorsReportStatementsService.getDirectorsReportStatements(TRANSACTION_ID, COMPANY_ACCOUNTS_ID))
+                .thenReturn(statementsApi);
+
+        when(directorsReportStatementsService.updateDirectorsReportStatements(TRANSACTION_ID, COMPANY_ACCOUNTS_ID, statementsApi))
+                .thenReturn(validationErrors);
+
+        List<ValidationError> returned = principalActivitiesService.submitPrincipalActivities(TRANSACTION_ID, COMPANY_ACCOUNTS_ID, principalActivities);
+
+        assertEquals(validationErrors, returned);
+
+        verify(directorsReportStatementsTransformer).setPrincipalActivities(statementsApi, principalActivities);
+    }
+}
