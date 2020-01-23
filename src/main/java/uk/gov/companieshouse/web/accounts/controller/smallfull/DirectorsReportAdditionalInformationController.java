1fdbb562cd445f3f57ad2a87c8cf2a222402535e
Jamie87 <jwillison@companieshouse.gov.uk>
2020-01-23 16:43:05 +0000

    complete additional information note

---
+++ src/main/java/uk/gov/companieshouse/web/accounts/controller/smallfull/DirectorsReportAdditionalInformationController.java
@@ -0,0 +1,119 @@
+package uk.gov.companieshouse.web.accounts.controller.smallfull;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import uk.gov.companieshouse.web.accounts.annotation.NextController;
+import uk.gov.companieshouse.web.accounts.annotation.PreviousController;
+import uk.gov.companieshouse.web.accounts.controller.BaseController;
+import uk.gov.companieshouse.web.accounts.controller.ConditionalController;
+import uk.gov.companieshouse.web.accounts.exception.ServiceException;
+import uk.gov.companieshouse.web.accounts.model.directorsreport.AdditionalInformation;
+import uk.gov.companieshouse.web.accounts.model.state.CompanyAccountsDataState;
+import uk.gov.companieshouse.web.accounts.model.state.DirectorsReportStatements;
+import uk.gov.companieshouse.web.accounts.service.smallfull.AdditionalInformationService;
+import uk.gov.companieshouse.web.accounts.validation.ValidationError;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@NextController(DirectorsReportReviewController.class)
+@PreviousController(DirectorsReportAdditionalInformationSelectionController.class)
+@RequestMapping("/company/{companyNumber}/transaction/{transactionId}/company-accounts/{companyAccountsId}/small-full/directors-report/additional-information")
+public class DirectorsReportAdditionalInformationController extends BaseController implements ConditionalController {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private AdditionalInformationService additionalInformationService;
+
+    private static final String ADDITIONAL_INFORMATION = "additionalInformation";
+
+    @Override
+    protected String getTemplateName() { return "smallfull/additionalInformation"; }
+
+    @GetMapping
+    public String getAdditionalInformation(@PathVariable String companyNumber,
+                                           @PathVariable String transactionId,
+                                           @PathVariable String companyAccountsId,
+                                           Model model) {
+
+        addBackPageAttributeToModel(model, companyNumber, transactionId, companyAccountsId);
+
+        try {
+            AdditionalInformation additionalInformation =
+                    additionalInformationService.getAdditionalInformation(transactionId, companyAccountsId);
+
+            model.addAttribute(ADDITIONAL_INFORMATION, additionalInformation);
+
+        } catch (ServiceException e) {
+
+            LOGGER.errorRequest(request, e.getMessage(), e);
+            return ERROR_VIEW;
+        }
+
+        return getTemplateName();
+
+    }
+
+    @PostMapping
+    public String submitAdditionalInformation(@PathVariable String companyNumber,
+                                              @PathVariable String transactionId,
+                                              @PathVariable String companyAccountsId,
+                                              @ModelAttribute(ADDITIONAL_INFORMATION) @Valid AdditionalInformation additionalInformation,
+                                              BindingResult bindingResult,
+                                              Model model) {
+
+        addBackPageAttributeToModel(model, companyNumber, transactionId, companyAccountsId);
+
+        if (bindingResult.hasErrors()) {
+            return getTemplateName();
+        }
+
+        try {
+            List<ValidationError> validationErrors =
+                    additionalInformationService.submitAdditionalInformation(transactionId, companyAccountsId, additionalInformation);
+
+            if (!validationErrors.isEmpty()) {
+                bindValidationErrors(bindingResult, validationErrors);
+                return getTemplateName();
+            }
+
+        } catch (ServiceException e) {
+
+            LOGGER.errorRequest(request, e.getMessage(), e);
+            return ERROR_VIEW;
+        }
+
+        return navigatorService.getNextControllerRedirect(this.getClass(), companyNumber, transactionId, companyAccountsId);
+
+
+    }
+
+    @Override
+    public boolean willRender(String companyNumber, String transactionId, String companyAccountsId)
+            throws ServiceException {
+
+        CompanyAccountsDataState companyAccountsDataState = getStateFromRequest(request);
+        return Optional.ofNullable(companyAccountsDataState)
+                .map(CompanyAccountsDataState::getDirectorsReportStatements)
+                .map(DirectorsReportStatements::getHasProvidedAdditionalInformation)
+                .orElse(false);
+
+    }
+
+
+
+
+
+}
---
+++ src/main/java/uk/gov/companieshouse/web/accounts/controller/smallfull/DirectorsReportAdditionalInformationSelectionController.java
@@ -0,0 +1,141 @@
+package uk.gov.companieshouse.web.accounts.controller.smallfull;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import uk.gov.companieshouse.web.accounts.annotation.NextController;
+import uk.gov.companieshouse.web.accounts.annotation.PreviousController;
+import uk.gov.companieshouse.web.accounts.api.ApiClientService;
+import uk.gov.companieshouse.web.accounts.controller.BaseController;
+import uk.gov.companieshouse.web.accounts.controller.ConditionalController;
+import uk.gov.companieshouse.web.accounts.exception.ServiceException;
+import uk.gov.companieshouse.web.accounts.model.directorsreport.AdditionalInformationSelection;
+import uk.gov.companieshouse.web.accounts.model.state.CompanyAccountsDataState;
+import uk.gov.companieshouse.web.accounts.model.state.DirectorsReportStatements;
+import uk.gov.companieshouse.web.accounts.service.smallfull.AdditionalInformationSelectionService;
+import uk.gov.companieshouse.web.accounts.service.smallfull.DirectorsReportService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+@NextController(DirectorsReportAdditionalInformationController.class)
+@PreviousController(PoliticalAndCharitableDonationsController.class)
+@RequestMapping("/company/{companyNumber}/transaction/{transactionId}/company-accounts/{companyAccountsId}/small-full/directors-report/additional-information-question")
+public class DirectorsReportAdditionalInformationSelectionController extends BaseController implements ConditionalController {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private AdditionalInformationSelectionService selectionService;
+
+    @Autowired
+    private DirectorsReportService directorsReportService;
+
+    @Autowired
+    private ApiClientService apiClientService;
+
+    private static final String ADDITIONAL_INFORMATION_SELECTION = "additionalInformationSelection";
+
+    @Override
+    protected String getTemplateName() { return "smallfull/additionalInformationSelection"; }
+
+    @GetMapping
+    public String getAdditionalInformationSelection(@PathVariable String companyNumber,
+                                                               @PathVariable String transactionId,
+                                                               @PathVariable String companyAccountsId,
+                                                               Model model) {
+
+        addBackPageAttributeToModel(model, companyNumber, transactionId, companyAccountsId);
+
+        try {
+            AdditionalInformationSelection selection =
+                    selectionService.getAdditionalInformationSelection(transactionId, companyAccountsId);
+
+            if (selection.getHasAdditionalInformation() == null) {
+                setHasProvidedAdditionalInformation(request, selection);
+            }
+
+            model.addAttribute(ADDITIONAL_INFORMATION_SELECTION, selection);
+        } catch (ServiceException e) {
+
+            LOGGER.errorRequest(request, e.getMessage(), e);
+            return ERROR_VIEW;
+        }
+
+        return getTemplateName();
+    }
+
+    @PostMapping
+    public String submitAdditionalInformationSelection(@PathVariable String companyNumber,
+                                                      @PathVariable String transactionId,
+                                                      @PathVariable String companyAccountsId,
+                                                      @ModelAttribute(ADDITIONAL_INFORMATION_SELECTION) @Valid AdditionalInformationSelection selection,
+                                                      BindingResult bindingResult,
+                                                      Model model) {
+
+        addBackPageAttributeToModel(model, companyNumber, transactionId, companyAccountsId);
+
+        if (bindingResult.hasErrors()) {
+            return getTemplateName();
+        }
+
+        try {
+            selectionService.submitAdditionalInformationSelection(transactionId, companyAccountsId, selection);
+
+        } catch (ServiceException e) {
+
+            LOGGER.errorRequest(request, e.getMessage(), e);
+            return ERROR_VIEW;
+        }
+
+        cacheHasProvidedAdditionalInformation(request, selection);
+
+        return navigatorService.getNextControllerRedirect(this.getClass(), companyNumber, transactionId, companyAccountsId);
+
+    }
+
+    private void setHasProvidedAdditionalInformation(HttpServletRequest request, AdditionalInformationSelection selection) {
+
+        CompanyAccountsDataState companyAccountsDataState = getStateFromRequest(request);
+        selection.setHasAdditionalInformation(
+                Optional.of(companyAccountsDataState)
+                    .map(CompanyAccountsDataState::getDirectorsReportStatements)
+                    .map(DirectorsReportStatements::getHasProvidedAdditionalInformation)
+                    .orElse(null));
+    }
+
+    private void cacheHasProvidedAdditionalInformation(HttpServletRequest request, AdditionalInformationSelection selection) {
+
+        CompanyAccountsDataState companyAccountsDataState = getStateFromRequest(request);
+
+        if (companyAccountsDataState.getDirectorsReportStatements() == null) {
+            companyAccountsDataState.setDirectorsReportStatements(new DirectorsReportStatements());
+
+        }
+
+        companyAccountsDataState.getDirectorsReportStatements().setHasProvidedAdditionalInformation(
+                selection.getHasAdditionalInformation());
+
+        updateStateOnRequest(request, companyAccountsDataState);
+    }
+
+
+
+    @Override
+    public boolean willRender(String companyNumber, String transactionId, String companyAccountsId)
+            throws ServiceException {
+
+        return directorsReportService.getDirectorsReport(apiClientService.getApiClient(), transactionId, companyAccountsId) != null;
+    }
+
+}
