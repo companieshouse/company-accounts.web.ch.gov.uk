1312be2e4aa0630462452052254714eed663ec23
bsaunders95 <bsaunders@companieshouse.gov.uk>
2020-03-09 13:25:03 +0000

    Add validation mapping and error message

--- src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/offbalancesheetarrangements/OffBalanceSheetArrangements.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/model/smallfull/notes/offbalancesheetarrangements/OffBalanceSheetArrangements.java
@@ -1,7 +1,14 @@
 package uk.gov.companieshouse.web.accounts.model.smallfull.notes.offbalancesheetarrangements;
 
+import javax.validation.constraints.NotBlank;
+import uk.gov.companieshouse.web.accounts.validation.ValidationMapping;
+import uk.gov.companieshouse.web.accounts.validation.ValidationModel;
+
+@ValidationModel
 public class OffBalanceSheetArrangements {
 
+    @NotBlank(message = "{offBalanceSheetArrangements.details.missing}")
+    @ValidationMapping("$.off_balance_sheet_arrangements.details")
     private String details;
 
     public String getDetails() {
