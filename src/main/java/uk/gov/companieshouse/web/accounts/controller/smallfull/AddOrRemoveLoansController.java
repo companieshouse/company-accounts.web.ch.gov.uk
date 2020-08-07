2fb0f7ce130616b14327445cc3c2b9a87b761e56
Faisal Amin <famin@companieshouse.gov.uk>
2020-08-07 11:01:05 +0100

    Initial stage of add loan

--- src/main/java/uk/gov/companieshouse/web/accounts/controller/smallfull/AddOrRemoveLoansController.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/controller/smallfull/AddOrRemoveLoansController.java
@@ -3,18 +3,30 @@ package uk.gov.companieshouse.web.accounts.controller.smallfull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.util.UriTemplate;
+import uk.gov.companieshouse.api.ApiClient;
+import uk.gov.companieshouse.api.model.accounts.smallfull.AccountingPeriodApi;
+import uk.gov.companieshouse.api.model.accounts.smallfull.SmallFullApi;
 import uk.gov.companieshouse.web.accounts.annotation.NextController;
 import uk.gov.companieshouse.web.accounts.annotation.PreviousController;
+import uk.gov.companieshouse.web.accounts.api.ApiClientService;
 import uk.gov.companieshouse.web.accounts.controller.BaseController;
 import uk.gov.companieshouse.web.accounts.exception.ServiceException;
+import uk.gov.companieshouse.web.accounts.model.directorsreport.AddOrRemoveDirectors;
 import uk.gov.companieshouse.web.accounts.model.loanstodirectors.AddOrRemoveLoans;
 import uk.gov.companieshouse.web.accounts.service.smallfull.LoanService;
+import uk.gov.companieshouse.web.accounts.service.smallfull.SmallFullService;
+import uk.gov.companieshouse.web.accounts.validation.ValidationError;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 @NextController(OffBalanceSheetArrangementsQuestionController.class)
@@ -25,7 +37,21 @@ public class AddOrRemoveLoansController extends BaseController {
     @Autowired
     private LoanService loanService;
 
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private SmallFullService smallFullService;
+
+    @Autowired
+    private ApiClientService apiClientService;
+
+    private static final UriTemplate URI =
+            new UriTemplate("/company/{companyNumber}/transaction/{transactionId}/company-accounts/{companyAccountsId}/small-full/add-or-remove-directors");
+
     private static final String ADD_OR_REMOVE_LOANS = "addOrRemoveLoans";
+
+    private static final String NEXT_ACCOUNTS = "nextAccounts";
 
     private static final String COMPANY_NUMBER = "companyNumber";
 
@@ -37,10 +63,23 @@ public class AddOrRemoveLoansController extends BaseController {
     public String getAddOrRemoveLoans(@PathVariable String companyNumber,
                                       @PathVariable String transactionId,
                                       @PathVariable String companyAccountsId,
-                                      Model model,
-                                      HttpServletRequest request) {
+                                      Model model) {
 
         addBackPageAttributeToModel(model, companyNumber, transactionId, companyAccountsId);
+
+        AccountingPeriodApi nextAccounts;
+
+        ApiClient apiClient = apiClientService.getApiClient();
+        try {
+            SmallFullApi smallFullApi = smallFullService.getSmallFullAccounts(apiClient, transactionId, companyAccountsId);
+
+            nextAccounts = smallFullApi.getNextAccounts();
+
+        } catch (ServiceException e) {
+
+            LOGGER.errorRequest(request, e.getMessage(), e);
+            return ERROR_VIEW;
+        }
 
         AddOrRemoveLoans addOrRemoveLoans = new AddOrRemoveLoans();
 
@@ -54,6 +93,8 @@ public class AddOrRemoveLoansController extends BaseController {
             return ERROR_VIEW;
         }
 
+
+        model.addAttribute(NEXT_ACCOUNTS, nextAccounts);
         model.addAttribute(ADD_OR_REMOVE_LOANS, addOrRemoveLoans);
         model.addAttribute(COMPANY_NUMBER, companyNumber);
         model.addAttribute(TRANSACTION_ID, transactionId);
@@ -71,6 +112,30 @@ public class AddOrRemoveLoansController extends BaseController {
         return navigatorService
                 .getNextControllerRedirect(this.getClass(), companyNumber, transactionId,
                         companyAccountsId);
+    }
+
+    @PostMapping(params = "add")
+    public String addLoan(@PathVariable String companyNumber,
+                              @PathVariable String transactionId,
+                              @PathVariable String companyAccountsId,
+                              @ModelAttribute(ADD_OR_REMOVE_LOANS) AddOrRemoveLoans addOrRemoveLoans,
+                              Model model) {
+
+        addBackPageAttributeToModel(model, companyNumber, transactionId, companyAccountsId);
+
+        try {
+
+            List<ValidationError> validationErrors = loanService.createLoan(transactionId, companyAccountsId, addOrRemoveLoans.getLoanToAdd());
+
+        } catch (ServiceException e) {
+
+            LOGGER.errorRequest(request, e.getMessage(), e);
+            return ERROR_VIEW;
+
+        }
+
+        return UrlBasedViewResolver.REDIRECT_URL_PREFIX +
+                URI.expand(companyNumber, transactionId, companyAccountsId).toString();
     }
 
     @Override
