94842bd40bca68f9d95e1db76b4393307de4d68d
mreis-ch <mreis@companieshouse.gov.uk>
2020-09-07 14:25:15 +0100

    Fix issue of navigation if loans to directors additional info question is No
    Update LoansToDirectorsAdditionalController to implement ConditionalController and add willRender method
    Update check in postLoansToDirectorsAdditionalInfo to check if the additional information details field is null and if so create otherwise update
    
    BI-4475

--- src/main/java/uk/gov/companieshouse/web/accounts/controller/smallfull/LoansToDirectorsAdditionalInfoController.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/controller/smallfull/LoansToDirectorsAdditionalInfoController.java
@@ -3,6 +3,7 @@ package uk.gov.companieshouse.web.accounts.controller.smallfull;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import org.apache.commons.lang.BooleanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,8 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import uk.gov.companieshouse.web.accounts.annotation.NextController;
 import uk.gov.companieshouse.web.accounts.annotation.PreviousController;
 import uk.gov.companieshouse.web.accounts.controller.BaseController;
+import uk.gov.companieshouse.web.accounts.controller.ConditionalController;
 import uk.gov.companieshouse.web.accounts.exception.ServiceException;
 import uk.gov.companieshouse.web.accounts.model.smallfull.notes.loanstodirectors.LoansToDirectorsAdditionalInfo;
+import uk.gov.companieshouse.web.accounts.model.state.CompanyAccountsDataState;
 import uk.gov.companieshouse.web.accounts.service.smallfull.LoansToDirectorsAdditionalInfoService;
 import uk.gov.companieshouse.web.accounts.validation.ValidationError;
 
@@ -24,12 +27,15 @@ import uk.gov.companieshouse.web.accounts.validation.ValidationError;
 @PreviousController(LoansToDirectorsAdditionalInfoQuestionController.class)
 @NextController(OffBalanceSheetArrangementsQuestionController.class)
 @RequestMapping("/company/{companyNumber}/transaction/{transactionId}/company-accounts/{companyAccountsId}/small-full/notes/add-or-remove-loans/additional-information")
-public class LoansToDirectorsAdditionalInfoController extends BaseController {
+public class LoansToDirectorsAdditionalInfoController extends BaseController implements ConditionalController {
 
     private static final String LOANS_TO_DIRECTORS_ADDITIONAL_INFO = "loansToDirectorsAdditionalInfo";
 
     @Autowired
     private LoansToDirectorsAdditionalInfoService loansToDirectorsAdditionalInfoService;
+
+    @Autowired
+    private HttpServletRequest request;
     
     @Override
     protected String getTemplateName() {
@@ -76,7 +82,7 @@ public class LoansToDirectorsAdditionalInfoController extends BaseController {
             LoansToDirectorsAdditionalInfo existingLoanToDirectorsAdditionalInfo = loansToDirectorsAdditionalInfoService.getAdditionalInformation(transactionId, companyAccountsId);
 
             List<ValidationError> validationErrors;
-            if(existingLoanToDirectorsAdditionalInfo == null) {
+            if(existingLoanToDirectorsAdditionalInfo.getAdditionalInfoDetails() == null) {
                 validationErrors = loansToDirectorsAdditionalInfoService.createAdditionalInformation(transactionId, companyAccountsId, loanToDirectorsAdditionalInfo);
             } else {
                 validationErrors = loansToDirectorsAdditionalInfoService.updateAdditionalInformation(transactionId, companyAccountsId, loanToDirectorsAdditionalInfo);
@@ -95,5 +101,13 @@ public class LoansToDirectorsAdditionalInfoController extends BaseController {
         return navigatorService
             .getNextControllerRedirect(this.getClass(), companyNumber, transactionId,
                 companyAccountsId);
+    }
+
+    @Override
+    public boolean willRender(String companyNumber, String transactionId, String companyAccountsId)
+            throws ServiceException {
+
+        CompanyAccountsDataState companyAccountsDataState = getStateFromRequest(request);
+        return BooleanUtils.isTrue(companyAccountsDataState.getHasIncludedLoansToDirectorsAdditionalInfo());
     }
 }
