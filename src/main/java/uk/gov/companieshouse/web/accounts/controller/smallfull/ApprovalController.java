d55f9760042d774fe6d9ec9cabe2df6b3bea0ef4
Faisal Amin <famin@companieshouse.gov.uk>
2021-09-22 13:29:29 +0100

    Add summary query parameter to the path to disable the summary screen Payment summary screen is shown by default. We do not need the summary screen for CIC payments anymore and providing the query parameter as false, the service goes to the payment page rather than summary screen.

--- src/main/java/uk/gov/companieshouse/web/accounts/controller/smallfull/ApprovalController.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/controller/smallfull/ApprovalController.java
@@ -44,6 +44,7 @@ public class ApprovalController extends BaseController {
     private static final String IS_PAYABLE_TRANSACTION = "isPayableTransaction";
 
     private static final UriTemplate SUBMITTED_ACCOUNTS_PATH = new UriTemplate("/company/{companyNumber}/transaction/{transactionId}/company-accounts/{companyAccountsId}/small-full/approved-accounts");
+    private static final String SUMMARY_SCREEN_FALSE = "?summary=false";
 
     @Autowired
     private TransactionService transactionService;
@@ -143,7 +144,7 @@ public class ApprovalController extends BaseController {
                 transactionService.updateResumeLink(transactionId, RESUME_URI.expand(companyNumber, transactionId, companyAccountsId).toString());
 
                 return UrlBasedViewResolver.REDIRECT_URL_PREFIX +
-                        paymentService.createPaymentSessionForTransaction(transactionId);
+                        paymentService.createPaymentSessionForTransaction(transactionId) + SUMMARY_SCREEN_FALSE;
             }
 
         } catch (ServiceException e) {
