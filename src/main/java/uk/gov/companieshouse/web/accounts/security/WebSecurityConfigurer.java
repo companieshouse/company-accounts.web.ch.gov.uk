bbadc3a5ad253c6911904dcff6692727fae8b1fa
dhawes-ch <dhawes@companieshouse.gov.uk>
2024-09-25 13:02:57 +0100

    filterchains

--- src/main/java/uk/gov/companieshouse/web/accounts/security/WebSecurityConfigurer.java
+++ src/main/java/uk/gov/companieshouse/web/accounts/security/WebSecurityConfigurer.java
@@ -12,34 +12,35 @@ import uk.gov.companieshouse.auth.filter.HijackFilter;
 import uk.gov.companieshouse.session.handler.SessionHandler;
 
 @EnableWebSecurity
+@Configuration
 public class WebSecurityConfigurer {
 
-    @Configuration
     @Order(1)
-    public static class GovUkAccountsSecurityFilterConfig {
+    @Bean
+    public SecurityFilterChain govUkAccountsSecurityFilterChain(HttpSecurity http)
+            throws Exception {
 
-        @Bean
-        public SecurityFilterChain govUkAccountsSecurityFilterChain(HttpSecurity http)
-                throws Exception {
-
-            return http.securityMatcher("/accounts/**")
-                    .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
-                    .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class).build();
-        }
+        return http.securityMatcher("/accounts/**")
+                .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class).build();
     }
 
-    @Configuration
-    public static class CompanyAccountsSecurityFilterConfig {
+    @Order(2)
+    @Bean
+    public SecurityFilterChain healthCheckSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http.securityMatcher("/company-accounts-web/healthcheck").authorizeHttpRequests(
+                        auth -> auth.requestMatchers("/company-accounts-web/healthcheck").permitAll())
+                .build();
+    }
 
-        @Bean
-        public SecurityFilterChain companyAccountsSecurityFilterChain(HttpSecurity http)
-                throws Exception {
-            http
-                    .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
-                    .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
-                    .addFilterBefore(new CompanyAuthFilter(), BasicAuthenticationFilter.class);
 
-            return http.build();
-        }
+    @Bean
+    public SecurityFilterChain companyAccountsSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        http.addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new CompanyAuthFilter(), BasicAuthenticationFilter.class);
+
+        return http.build();
     }
 }
