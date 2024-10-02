d20e7cff6f4ba97d43081713aa34bc7202128328
tmoller-ch <tmoller@companieshouse.gov.uk>
2024-10-02 10:06:41 +0100

    update healthcheck grace period

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -9,7 +9,6 @@ locals {
   kms_alias                  = "alias/${var.aws_profile}/environment-services-kms"
   lb_listener_rule_priority  = 300
   lb_listener_paths      = ["/accounts/*","/company/*/*/criteria","/company/*/small-full/steps-to-complete","/company/*/corporation-tax","/company/*/select-account-type","/company/*/cic/steps-to-complete","/company/*/transaction/*/company-accounts/*"]
-
   healthcheck_path           = "/company-accounts-web/healthcheck" #healthcheck path for company accounts web
   healthcheck_matcher        = "200"
   s3_config_bucket           = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
